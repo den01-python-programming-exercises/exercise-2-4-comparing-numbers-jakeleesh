c26e72f7214f881efdcfcff40bf8cc3ec02461a1
Jake Lee <jake.lee@fdmgroup.com>
2021-09-01 21:16:30 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    num1 = int(input())
+    num2 = int(input())
+    if (num1 > num2):
+        print(str(num1) + " is greater than " + str(num2) + ".")
+    elif (num1 < num2):
+        print(str(num1) + " is smaller than " + str(num2) + ".")
+    else:
+        print(str(num1) + " is equal to " + str(num2) + ".")
 
 if __name__ == '__main__':
     main()
